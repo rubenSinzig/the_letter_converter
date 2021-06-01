d07c601e4ac39603125ee38ae8a81c5df465cd7a
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-01 14:24:47 +0200

    wrapper-footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Input />
       <Output />
-      {/* <Footer /> */}
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  // add here var and function for Copyright year
+  return (
+    <div className="wrapper-footer">
+      Copyright<sup>©</sup> <span className="year"></span>
+    </div>
+  );
+};
+export default Footer;
