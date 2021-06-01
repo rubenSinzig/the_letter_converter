2354421028fd24db9612244a8c988877930d14f6
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-01 14:16:22 +0200

    wrapper-output

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,4 +1,14 @@
 const Output = () => {
-  return <div className="wrapper-output"></div>;
+  return (
+    <div className="wrapper-output">
+      <div>
+        <h3>Morse Code</h3>
+      </div>
+      <textarea
+        placeholder="Converted text to Morse Code..."
+        readOnly
+      ></textarea>
+    </div>
+  );
 };
 export default Output;
