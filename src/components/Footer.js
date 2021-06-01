2d19710143de7d0ae2949ec06c63c796f25f07d5
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-01 15:49:03 +0200

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 const Footer = () => {
   // to get the full year
   const year = new Date().getFullYear();
-  document.querySelector(".year").innerHTML = year;
 
   return (
     <div className="wrapper-footer">
-      Copyright<sup>©</sup> <span className="year"></span>
+      Copyright<sup>©</sup> <span className="year">{year}</span>
     </div>
   );
 };
