b79da4864917053cc52e02ed9ca73f4b6ee68d33
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-01 15:43:55 +0200

    obj data.json

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Header from "./components/Header";
 import Input from "./components/Input";
 import Output from "./components/Output";
 import Footer from "./components/Footer";
+import Data from "./data.json";
 
 function App() {
+  // useState
+  const [input, setInput] = useState([]);
+  console.log(Data);
+
   return (
     <React.Fragment>
       <Header />
-      <Input />
-      <Output />
+      <Input setInput={setInput} />
+      <Output input={input} />
       <Footer />
     </React.Fragment>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,25 @@
-const Input = () => {
+import { useState } from "react";
+
+const Input = (props) => {
+  // useState
+  const [userInput, setUserInput] = useState("");
+
+  // this function ensures that the user can see what he/she types
+  function changeHandle(e) {
+    setUserInput(e.target.value);
+  }
+
   return (
     <div className="wrapper-input">
       <div>
         <h3>Text</h3>
       </div>
-      <textarea placeholder="Type your text here..."></textarea>
+      <textarea
+        type="text"
+        placeholder="Type your text here..."
+        value={userInput}
+        onChange={changeHandle}
+      ></textarea>
     </div>
   );
 };
